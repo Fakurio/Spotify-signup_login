bad39fb7f2e817dc8f851e7f3399fac90133a202
Kamil <kamil.zelazowski2@gmail.com>
2023-03-20 22:37:09 +0100

    firebase keys export

--- src/modules/AppModule/services/firebase-config.ts
+++ src/modules/AppModule/services/firebase-config.ts
@@ -10,5 +10,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+export default firebaseConfig
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
